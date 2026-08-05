11385c11527c48bf6e5d1c8f1135baca0707cb72
Cas0909 <a0910220691@gmail.com>
2026-08-06 02:31:05 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 alert("script 有執行");
 
-const random =
-cards[
-    Math.floor(Math.random() * cards.length)
+const random = cards[
+  Math.floor(Math.random() * cards.length)
 ];
+
+console.log(random);
+
+document.querySelector(".subtitle").textContent = random.text;
+
+document.querySelector("button").textContent =
+  random.actions[
+    Math.floor(Math.random() * random.actions.length)
+  ];
