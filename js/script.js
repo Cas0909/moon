9e5a55d36d94cc25eb05d0f51a4269c26f2af91b
Cas0909 <a0910220691@gmail.com>
2026-08-06 03:16:12 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-alert(window.cards.length);
+const random = window.cards[
+    Math.floor(Math.random() * window.cards.length)
+];
 
-function showCard() {
+document.querySelector(".subtitle").textContent = random.text;
 
-    const random = window.cards[
-        Math.floor(Math.random() * window.cards.length)
+document.querySelector("button").textContent =
+    random.actions[
+        Math.floor(Math.random() * random.actions.length)
     ];
-
-    document.querySelector(".subtitle").textContent = random.text;
-
-    document.querySelector("button").textContent =
-        random.actions[
-            Math.floor(Math.random() * random.actions.length)
-        ];
-}
-
-
-showCard();
