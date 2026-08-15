713e0e83bf41332729aff655895f14d90fe6d5c5
Cas0909 <a0910220691@gmail.com>
2026-08-15 13:12:18 +0800

    Enable welcome screen

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+const homeScreen = document.querySelector("#home-screen");
+const welcomeScreen = document.querySelector("#welcome-screen");
+const enterBtn = document.querySelector("#enterBtn");
+
 const random = window.cards[
     Math.floor(Math.random() * window.cards.length)
 ];
 
-document.querySelector(".subtitle").textContent = random.text;
+homeScreen.querySelector(".subtitle").textContent = random.text;
 
-document.querySelector("button").textContent =
+enterBtn.textContent =
     random.actions[
         Math.floor(Math.random() * random.actions.length)
     ];
+
+enterBtn.addEventListener("click", () => {
+    homeScreen.classList.add("hidden");
+    welcomeScreen.classList.remove("hidden");
+});
