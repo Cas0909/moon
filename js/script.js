d5bc07185f368d81f288d6dee8101c52c053ba9b
Cas0909 <a0910220691@gmail.com>
2026-08-15 13:25:10 +0800

    Connect introduction screen

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const homeScreen = document.querySelector("#home-screen");
 const welcomeScreen = document.querySelector("#welcome-screen");
+const introScreen = document.querySelector("#intro-screen");
+
 const enterBtn = document.querySelector("#enterBtn");
+const continueBtn = document.querySelector("#continueBtn");
 
 const random = window.cards[
     Math.floor(Math.random() * window.cards.length)
@@ -16,4 +19,9 @@ enterBtn.textContent =
 enterBtn.addEventListener("click", () => {
     homeScreen.classList.add("hidden");
     welcomeScreen.classList.remove("hidden");
+});
+
+continueBtn.addEventListener("click", () => {
+    welcomeScreen.classList.add("hidden");
+    introScreen.classList.remove("hidden");
 });
